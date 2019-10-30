ac1f8cb58a50f149f9f92c634ee9de79ff2fdb23
Yevgeniy Vakhroushev <Vea@Yevgeniys-MacBook-Pro-2.local>
2019-10-30 07:13:00 -0400

    again

---
+++ day-01-p-2/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	s := bufio.NewScanner(f)
+	m := map[int]int{}
+	nums := make([]int, 0)
+	var total int
+
+	for s.Scan() {
+		var num int
+		fmt.Sscanf(s.Text(), "%d", &num)
+		nums = append(nums, num)
+		total += num
+		m[total]++
+		if m[total] > 1 {
+			log.Fatalf("ans: %d\n", total)
+		}
+	}
+
+	if err := s.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		for _, num := range nums {
+			total += num
+			m[total]++
+			if m[total] > 1 {
+				log.Fatalf("ans: %d\n", total)
+			}
+		}
+	}
+}
---
+++ day-02-p-1/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	rr := bufio.NewReader(f)
+	var twos, threes int
+	var t, th int
+	m := map[rune]int{}
+	for {
+		r, _, err := rr.ReadRune()
+		if err != nil && err != io.EOF {
+			log.Fatalf("cound not read rune from file: %s", err)
+		}
+
+		if r == '\n' || err == io.EOF {
+			if t > 0 {
+				twos++
+			}
+			if th > 0 {
+				threes++
+			}
+			if err == io.EOF {
+				break
+			}
+			t, th = 0, 0
+			m = map[rune]int{}
+		}
+		m[r]++
+		switch m[r] {
+		case 2:
+			t++
+		case 3:
+			t--
+			th++
+		default:
+			if m[r] > 3 {
+				th--
+			}
+		}
+
+	}
+	fmt.Println(twos * threes)
+}
